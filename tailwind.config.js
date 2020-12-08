a6e948d32ab91022edbb0d8e904571cf69d6a06b
Jordi Tarrida <jordi.tarrida@gmail.com>
2020-12-08 13:23:42 +0100

    customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        teal: colors.teal
+      },
       container: {
         center: true,
         padding: "1rem"
